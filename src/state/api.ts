d0684fa64d460c4f7aaa3e930bad60e3b785f4a3
RaamVijith <raamvijith1@gmail.com>
2024-12-29 20:27:39 +0530

    complete redux and side,Nav bar

---
+++ src/state/api.ts
@@ -0,0 +1,10 @@
+import { createApi, CreateApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+export const api = createApi({
+    baseQuery: fetchBaseQuery({ baseUrl: process.env.NEXT_PUBLIC_API_URL }),
+    reducerPath: "api",
+    tagTypes: [],
+    endpoints: (builder) => ({}),
+})
+
+export const {} = api;
